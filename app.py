4a8124dec03f828dd0feab2135491778b49c0ce6
medfit-2026 <sona54.bachurina54@gmail.com>
2026-09-24 02:11:56 +1000

    Add asyncio event loop for running the bot

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import os
+import asyncio
 from bot_logic import run_telegram_bot
 
 if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     run_telegram_bot()
