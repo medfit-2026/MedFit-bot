0a2a2389d4477cf80e44cd976ebbe8dadbd9cce1
medfit-2026 <sona54.bachurina54@gmail.com>
2026-09-24 02:05:34 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,5 @@
 import os
-import asyncio
-import threading
-from flask import Flask
 from bot_logic import run_telegram_bot
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return "Bot is running"
-
-
-@app.route('/health')
-def health():
-    return "OK"
-
-
-def start_bot():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    run_telegram_bot()
-
-
 if __name__ == "__main__":
-    bot_thread = threading.Thread(target=start_bot, daemon=True)
-    bot_thread.start()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    run_telegram_bot()
